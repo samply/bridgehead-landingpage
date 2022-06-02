785820582c1646853dd53d3e2aad2b450fd7241a
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2022-06-02 09:42:02 +0200

    Added c4

---
+++ projects/c4.py
@@ -0,0 +1,13 @@
+from common import Service, ServiceGroup, ServiceType
+
+projectname_friendly = "Clinical Communication Platform"
+
+services = [
+    Service(ServiceGroup.PSEUDONYMIZATION, "Zentraler Kontrollnummernerzeuger", "https://dktk-kne.kgu.de", ServiceType.CENTRAL),
+    Service(ServiceGroup.PSEUDONYMIZATION, "Zentrale Patientenliste", "https://patientlist.ccp-it.dktk.dkfz.de", ServiceType.CENTRAL),
+    Service(ServiceGroup.SEARCH, "Zentrale Suche", "https://centralsearch.ccp-it.dktk.dkfz.de", ServiceType.CENTRAL),
+    Service(ServiceGroup.SEARCH, "Dezentrale Suche", "https://decentralsearch.ccp-it.dktk.dkfz.de", ServiceType.CENTRAL),
+    Service(ServiceGroup.OPERATIONS, "Deployment-Server", "https://deployment.ccp-it.dktk.dkfz.de", ServiceType.CENTRAL),
+    Service(ServiceGroup.OPERATIONS, "Zentrales Monitoring", "https://monitor.vmitro.de/icingaweb2/", ServiceType.CENTRAL),
+    Service(ServiceGroup.OPERATIONS, "Reverse Proxy (Traefik)", "https://<THISHOST>/dashboard", ServiceType.BRIDGEHEAD)
+]
