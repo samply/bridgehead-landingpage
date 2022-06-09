4d5b6aeb482ee4517fabef4be94173caea870d62
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2022-06-09 15:52:37 +0200

    Added missing trailing / on dashboard

--- projects/ccp.py
+++ projects/ccp.py
@@ -9,7 +9,7 @@ services = [
     Service(ServiceGroup.SEARCH, "Dezentrale Suche", "https://decentralsearch.ccp-it.dktk.dkfz.de", ServiceType.CENTRAL),
     Service(ServiceGroup.OPERATIONS, "Deployment-Server", "https://deployment.ccp-it.dktk.dkfz.de", ServiceType.CENTRAL),
     Service(ServiceGroup.OPERATIONS, "Zentrales Monitoring", "https://monitor.vmitro.de/icingaweb2/", ServiceType.CENTRAL),
-    Service(ServiceGroup.OPERATIONS, "Reverse Proxy (Traefik)", "https://<THISHOST>/dashboard", ServiceType.BRIDGEHEAD),
+    Service(ServiceGroup.OPERATIONS, "Reverse Proxy (Traefik)", "https://<THISHOST>/dashboard/", ServiceType.BRIDGEHEAD),
     Service(ServiceGroup.SEARCH, "Connector(Teiler)", "https://<THISHOST>/ccp-connector", ServiceType.BRIDGEHEAD),
     Service(ServiceGroup.PERSISTENCE, "Samply Store", "https://<THISHOST>/ccp-localdatamanagement", ServiceType.BRIDGEHEAD),
     Service(ServiceGroup.SEARCH, "ID-Manager", "https://<THISHOST>/idmanager", ServiceType.BRIDGEHEAD)
