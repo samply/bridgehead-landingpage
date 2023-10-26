75395a9d43f8082719ed6df51c5a827c4f9cfce3
Patrick Skowronek <patrick.skowronek@dkfz-heidelberg.de>
2023-10-26 11:14:13 +0200

    Switched link to monitoring

--- projects/ccp.py
+++ projects/ccp.py
@@ -8,7 +8,7 @@ services = [
     Service(ServiceGroup.SEARCH, "Zentrale Suche", "https://centralsearch.ccp-it.dktk.dkfz.de", ServiceType.CENTRAL),
     Service(ServiceGroup.SEARCH, "Dezentrale Suche", "https://decentralsearch.ccp-it.dktk.dkfz.de", ServiceType.CENTRAL),
     Service(ServiceGroup.OPERATIONS, "Deployment-Server", "https://deployment.ccp-it.dktk.dkfz.de", ServiceType.CENTRAL),
-    Service(ServiceGroup.OPERATIONS, "Zentrales Monitoring", "https://monitor.vmitro.de/icingaweb2/", ServiceType.CENTRAL),
+    Service(ServiceGroup.OPERATIONS, "Zentrales Monitoring", "https://monitoring.verbis.dkfz.de/icingaweb2/dashboard", ServiceType.CENTRAL),
     Service(ServiceGroup.OPERATIONS, "Reverse Proxy (Traefik)", "https://<THISHOST>/dashboard/", ServiceType.BRIDGEHEAD),
     Service(ServiceGroup.PERSISTENCE, "FHIR-Server (Blaze)", "https://<THISHOST>/ccp-localdatamanagement/fhir", ServiceType.BRIDGEHEAD)
 ]
